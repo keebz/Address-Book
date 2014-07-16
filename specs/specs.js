d6cc6fce77a4a06aa3d2741348c74c56076b9f9e
Lee Kebler & Kyle Smith <student@epicodus.com>
2014-07-16 10:39:34 -0700

    followed along with the lesson and finished the first version of the address book.

--- specs/specs.js
+++
@@ -1,5 +0,0 @@
-describe ('contact', function(){
-  it ("when a first and last name is entered, the page returns the first and last name", function(){
-  contact("Robert", "Downey").should.eql("Robert Downey");
-  });
-});
